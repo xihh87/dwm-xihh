358e93480f38361a29524bc79cd812c33d23c179
Joshua Haase <hahj87@gmail.com>
2016-08-16 11:24:46 -0500

    Use super key as modifier.

--- config.def.h
+++ config.def.h
@@ -47,7 +47,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define NOMODKEY 0
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
