fe5cda6ad33ba897732e2650d679cf705c228794
Joshua Haase <hahj87@gmail.com>
2017-09-09 18:31:19 -0500

    Use history on dmenu_run.

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "/usr/bin/dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "/usr/bin/env", "SHELL=/usr/bin/fish", "st", "-e", "dvtm", NULL };
 static const char *emcscmd[] = {"/usr/bin/emacs", NULL };
 static const char *filebwsr2[] = {"/usr/bin/nautilus", NULL };
