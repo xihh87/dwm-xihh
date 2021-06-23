62fdefcba24f412bce86116a6dcf481040cea880
Joshua Haase <hahj87@gmail.com>
2021-06-23 18:34:41 -0500

    Switch rows and cols behavior on grid mode.

--- layouts.c
+++ layouts.c
@@ -40,20 +40,20 @@ grid(Monitor *m) {
 		n++;
 
 	/* grid dimensions */
-	for(rows = 0; rows <= n/2; rows++)
-		if(rows*rows >= n)
+	for(cols = 0; cols <= n/2; cols++)
+		if(cols*cols >= n)
 			break;
-	cols = (rows && (rows - 1) * rows >= n) ? rows - 1 : rows;
+	rows = (cols && (cols - 1) * cols >= n) ? cols - 1 : cols;
 
 	/* window geoms (cell height/width) */
-	ch = m->wh / (rows ? rows : 1);
-	cw = m->ww / (cols ? cols : 1);
+	ch = m->wh / (cols ? cols : 1);
+	cw = m->ww / (rows ? rows : 1);
 	for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
-		cx = m->wx + (i / rows) * cw;
-		cy = m->wy + (i % rows) * ch;
+		cx = m->wx + (i / cols) * cw;
+		cy = m->wy + (i % cols) * ch;
 		/* adjust height/width of last row/column's windows */
-		ah = ((i + 1) % rows == 0) ? m->wh - ch * rows : 0;
-		aw = (i >= rows * (cols - 1)) ? m->ww - cw * cols : 0;
+		ah = ((i + 1) % cols == 0) ? m->wh - ch * cols : 0;
+		aw = (i >= cols * (rows - 1)) ? m->ww - cw * rows : 0;
 		resize(c, cx, cy, cw - 2 * c->bw + aw, ch - 2 * c->bw + ah, False);
 		i++;
 	}
